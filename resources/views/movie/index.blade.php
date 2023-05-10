80dfcebab5c7b8944213eadd05f651156d088d12
MarcoCocco <marcococco28@gmail.com>
2023-05-10 15:13:54 +0200

    feat: recuperati i film dal database con le funzioni e passate alla view + stilizzazione tabella bootstrap

--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -1,12 +1,28 @@
 @extends ('layouts/main')
+<h2>Lista dei Film</h2>
 
 @section('content')
-  Tutti i Film:
-  <ul>
-    @foreach ($movies as $movie)
-      <li>
-        {{$movie->title}} - {{$movie->original_title}} - {{$movie->nationality}} - {{$movie->date}} - {{$movie->vote}} 
-      </li>        
-    @endforeach
-  </ul>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Titolo</th>
+                <th scope="col">Titolo Originale</th>
+                <th scope="col">Nazionalità</th>
+                <th scope="col">Data di Uscita</th>
+                <th scope="col">Voto</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($movies as $movie)
+                <tr>
+                    <td>{{ $movie->title }}</td>
+                    <td>{{ $movie->original_title }}</td>
+                    <td>{{ $movie->nationality }}</td>
+                    <td>{{ $movie->date }}</td>
+                    <td>{{ $movie->vote }} </td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
 @endsection
